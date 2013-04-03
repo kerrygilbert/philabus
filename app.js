38f872bc0e8533ea409ccb75848b7c0c6b9b9fc3
Kerry Gilbert <kerry@punkave.com>
2013-04-03 15:56:33 -0400

    set up a test express route that takes a bus number as a request param and uses node-septa to grab some json

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-
-/**
- * Module dependencies.
- */
+//creating an empty app variable:
+var app;
 
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
   , path = require('path')
-  , nunjucks = require('nunjucks');
+  , nunjucks = require('nunjucks')
+  , septa = require('node-septa')();
 
-var app = express();
+//defining app
+app = express();
 
+//setting up nunjucks
 var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('views'));
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  // app.set('view engine', 'ejs');
 
   //feed app to nunjucks environment
   env.express(app);
@@ -37,7 +37,15 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+
 app.get('/', routes.index);
+
+app.get('/bus/:bus', function(req, res){
+  //call septa's getBus method (returns JSON) and output the response in our browser
+  septa.getBus(req.params.bus, function(data){
+    res.send(data);
+  });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
---
+++ public/javascripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  // if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(locateSuccess, locateFail);
+  // }
+  // else {
+  //     alert('I\'m sorry, but Geolocation is not supported in your current browser.');
+  // }
+  console.log('client side js is working just fine');
+});
