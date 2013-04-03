fadcc0893f65c53f6982497cc650517858a79320
Kerry Gilbert <kerry@punkave.com>
2013-04-03 12:00:33 -0400

    setup basic express app using sessions / ejs / less. swapped out ejs for nunjucks as the view engine. created basic layout.html and an index page that extends it.

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , nunjucks = require('nunjucks');
+
+var app = express();
+
+var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('views'));
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  // app.set('view engine', 'ejs');
+
+  //feed app to nunjucks environment
+  env.express(app);
+
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+
+  app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
